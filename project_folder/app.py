d2baf7b735998412aab1655f970e818aa8f7a3ed
SAI_KUMAR_ALLA <2000avsaikumar@gmail.com>
2025-05-02 15:09:12 +0530

    new commited diffent one

--- project_folder/app.py
+++ project_folder/app.py
@@ -9,3 +9,12 @@ def function(a,b,c,d):
 print(function(3,4,5,8))
 
 
+def function1(a,b,c,d,f):
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    d=int(d)
+    e =int(f)
+    return a+b+c+d+e
+
+print(function1(4,5,6,7,8))
